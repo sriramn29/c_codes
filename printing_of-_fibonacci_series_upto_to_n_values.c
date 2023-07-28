beac442ac16b98831623955a25e28d68a66ff3ec
sriramn29 <139962755+sriramn29@users.noreply.github.com>
2023-07-28 15:54:20 +0530

    Update printing_of-_fibonacci_series_upto_to_n_values.c

--- printing_of-_fibonacci_series_upto_to_n_values.c
+++ printing_of-_fibonacci_series_upto_to_n_values.c
@@ -1,26 +1,13 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
     int n,i;
-    long long int fib1=0,nextterm=0;
-    long long int fib2=fib1+1;
-    //scanf("%ld",&fib1);
     scanf("%d",&n);
     int a[n];
-    for(i=0;i<n;i++){
-        a[i]=fib1;
-        nextterm=fib1+fib2;
-        fib1=fib2;
-        fib2=nextterm;
+    a[0]=0,a[1]=1;
+    for(i=2;i<=n;i++){
+        a[i]=a[i-1]+a[i-2];
     }
     for(i=0;i<n;i++){
         printf("%d ",a[i]);
